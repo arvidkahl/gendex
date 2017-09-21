88a5acdfb61d1099b044bf252c6b4d0cc108b90f
Adolfo Silva <a9126@alunos.ipca.pt>
2017-09-21 16:07:19 +0100

    No more warnings

--- mix.exs
+++ mix.exs
@@ -9,15 +9,15 @@ defmodule Gendex.Mixfile do
     [app: :gendex,
      version: @version,
      name: "Gendex",
-     package: package,
+     package: package(),
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      source_url: @url,
      homepage_url: @url,
-     description: description,
-     deps: deps,
-     docs: docs]
+     description: description(),
+     deps: deps(),
+     docs: docs()]
   end
 
   # Configuration for the OTP application
@@ -28,25 +28,25 @@ defmodule Gendex.Mixfile do
      applications: [:logger]]
   end
 
-  defp deps do
+  defp deps() do
     [{:credo, "~> 0.3", only: [:dev, :test]},
      {:ex_doc, "~> 0.1", only: :dev},
      {:earmark, ">= 0.0.0", only: :dev}]
   end
 
-  defp docs do
-    [extras: docs_extras, main: "readme"]
+  defp docs() do
+    [extras: docs_extras(), main: "readme"]
   end
 
-  defp docs_extras do
+  defp docs_extras() do
     ["README.md"]
   end
 
-  defp description do
+  defp description() do
     "Gendex tells you the most likely gender of a person based on first name."
   end
 
-  defp package do
+  defp package() do
     [files: ["lib", "priv", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["andö"],
       licenses: ["MIT"],
